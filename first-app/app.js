b2693c86baffaad2c20b967d8e8480c797b8e4e8
Chao Fan <fanchao05@gmail.com>
2018-07-23 23:51:24 -0400

    learned more node modules

--- first-app/app.js
+++ first-app/app.js
@@ -1,3 +1,17 @@
-const log = require('./logger');
+const path = require('path');
+const os = require('os');
 
+// import module
+const log = require('./logger');
 log('message');
+
+// path module
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+
+// os module
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log('Total Memory: ' + totalMemory);
+console.log(`Free Memory: ${freeMemory}`);
