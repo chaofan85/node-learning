5f4c2c72b1ad512e5a18aa9311416a578d8db060
Chao Fan <fanchao05@gmail.com>
2018-07-24 15:28:05 -0400

    worked with file system

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
 
 // import module
 const log = require('./logger');
@@ -15,3 +16,13 @@ const freeMemory = os.freemem();
 
 console.log('Total Memory: ' + totalMemory);
 console.log(`Free Memory: ${freeMemory}`);
+
+// file system module
+
+fs.readdir('./', function(err, files) {
+  if (err) {
+    console.log('Error ', err);
+  } else {
+    console.log('Result', files);
+  }
+});
