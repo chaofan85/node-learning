c3fb1a73dbc5f64201811b9825fa37325c603a80
Chao Fan <fanchao05@gmail.com>
2018-07-25 21:25:13 -0400

    added different http requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: 'course1' },
   { id: 2, name: 'course2' },
@@ -15,7 +17,20 @@ app.get('/api/courses', (req, res) => {
   res.send(courses);
 });
 
-app.post('/api/courses', (req, res) => {});
+app.post('/api/courses', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    //400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 characters');
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
