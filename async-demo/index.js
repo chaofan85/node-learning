4a711af9e3975483f00e7451ccff73fc7670fe67
Chao Fan <fanchao05@gmail.com>
2018-07-28 16:23:50 -0400

    practice callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,25 @@
 console.log('Before');
 
-setTimeout(() => {
-  console.log('Reading a user from a database...');
-}, 2000);
+getUser(1, user => {
+  console.log('User: ', user);
+
+  getRepositories(user.gitHubUsername, repos => {
+    console.log('Repos: ', repos);
+  });
+});
 
 console.log('After');
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    callback({ id: id, gitHubUsername: 'mosh' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+}
