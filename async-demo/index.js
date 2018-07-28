6f6fadbcdf7afd64e1599cf200e67172a0add8c1
Chao Fan <fanchao05@gmail.com>
2018-07-28 17:02:19 -0400

    added async/await approach

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,10 +1,25 @@
 console.log('Before');
 
+// Promise-based approach
 getUser(1)
   .then(user => getRepositories(user.gitHubUsername))
   .then(repos => getCommits(repos[0]))
   .then(commits => console.log('commits: ', commits))
   .catch(error => console.log('Error: ', error.message));
+
+// Async and Await approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log('Error: ', err.message);
+  }
+}
+
+displayCommits();
 
 console.log('After');
 
