20340f7cc1f183ac971fa36b72662f0d9dc6807b
Chao Fan <fanchao05@gmail.com>
2018-07-29 22:24:20 -0400

    finished the exercise

---
+++ exercise-1/index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/mongo-exercises')
+  .then(() => console.log('Connected to mongoDB'))
+  .catch(err => console.error('Could not connect to mongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return await Course.find({ isPublished: true, tags: 'backend' })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
