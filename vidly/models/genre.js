a3fc8cf74e95399cb2851dbb7f728cd141ed39d8
Chao Fan <fanchao05@gmail.com>
2018-09-01 23:23:25 -0400

    added user model for Vidly

--- vidly/models/genre.js
+++ vidly/models/genre.js
@@ -14,12 +14,14 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = {
-    name: Joi.string().min(3).required()
+    name: Joi.string()
+      .min(3)
+      .required()
   };
 
   return Joi.validate(genre, schema);
 }
 
 exports.genreSchema = genreSchema;
-exports.Genre = Genre; 
+exports.Genre = Genre;
 exports.validate = validateGenre;
---
+++ vidly/models/user.js
@@ -0,0 +1,50 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 1024
+    }
+  })
+);
+
+function validateGenre(user) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(8)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateGenre;
