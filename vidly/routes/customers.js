24d049dc908bf0b0853b9db99a361f86daba8ba3
Chao Fan <fanchao05@gmail.com>
2018-09-01 23:15:58 -0400

    updated vidly

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const {Customer, validate} = require('../models/customer'); 
 const mongoose = require('mongoose');
+const express = require('express');
 const router = express.Router();
-const { Customer, validate } = require('../models/customer');
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -9,49 +9,39 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new Customer({
+  let customer = new Customer({ 
     name: req.body.name,
-    phone: req.body.phone,
-    isGold: req.body.isGold
+    isGold: req.body.isGold,
+    phone: req.body.phone
   });
-
-  customer = await customers.save();
-
+  customer = await customer.save();
+  
   res.send(customer);
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findByIdAndUpdate(
-    req.params.id,
-    {
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
       name: req.body.name,
       isGold: req.body.isGold,
       phone: req.body.phone
-    },
-    { new: true }
-  );
+    }, { new: true });
 
-  if (!customer)
-    return res.status(404).send('The customer with the given ID is not found');
-
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+  
   res.send(customer);
 });
 
 router.delete('/:id', async (req, res) => {
   const customer = await Customer.findByIdAndRemove(req.params.id);
 
-  if (!customer)
-    return res.status(404).send('The customer with the given ID is not found');
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
   res.send(customer);
 });
@@ -59,10 +49,9 @@ router.delete('/:id', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const customer = await Customer.findById(req.params.id);
 
-  if (!customer)
-    return res.status(404).send('The customer with the given ID is not found');
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
   res.send(customer);
 });
 
-module.exports = router;
+module.exports = router; 
