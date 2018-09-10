ef80acea0ee05c019285ae083d00d33a8833a8ae
Chao Fan <fanchao05@gmail.com>
2018-09-09 20:13:14 -0400

    updated auth

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const Joi = require("joi");
 const router = express.Router();
-const { User } = require('../models/user');
+const { User } = require("../models/user");
+const config = require("config");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+  if (!user) return res.status(400).send("Invalid email or password.");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
 
   res.send(token);
 });
